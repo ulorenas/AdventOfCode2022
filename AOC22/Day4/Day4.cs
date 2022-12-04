32563e259890e8e0f2cd2a2a035d5e360d360387
Lorenas <urnikislorenas@gmail.com>
2022-12-04 17:23:40 +0200

    complete day4

---
+++ AOC22/Day4/Day4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC22.Day4
+{
+    internal class Day4 : Day
+    {
+        public Day4(string path) : base(path)
+        {
+        }
+
+        public override void Execute()
+        {
+            var lines = File.ReadAllLines(@$"{Path}\data-day4.txt");
+            var result = lines
+                .Select(line => ToPairs(line))
+                .Count(pair => FullyContains(pair[0], pair[1]));
+
+            Console.WriteLine($"Amount of pairs whose range fully contain the other: {result}");
+
+            result = lines
+                .Select(line => ToPairs(line))
+                .Count(pair => Overlaps(pair[0], pair[1]));
+
+            Console.WriteLine($"Amount of pairs whose range overlaps the other: {result}");
+        }
+
+        int[][] ToPairs(string line)
+        {
+            return line
+                .Split(',')
+                .Select(assignment => assignment.Split('-').Select(num => int.Parse(num)).ToArray())
+                .ToArray();
+        }
+
+        bool FullyContains(int[] pair1, int[] pair2)
+        {
+            return
+                pair1[0] >= pair2[0] && pair1[1] <= pair2[1] ||
+                pair2[0] >= pair1[0] && pair2[1] <= pair1[1];
+        }
+
+        bool Overlaps(int[] pair1, int[] pair2)
+        {
+            return
+                pair1[0] >= pair2[0] && pair1[0] <= pair2[1] ||
+                pair1[1] >= pair2[0] && pair1[1] <= pair2[1] ||
+                pair2[0] >= pair1[0] && pair2[0] <= pair1[1] ||
+                pair2[1] >= pair1[0] && pair2[1] <= pair1[1];
+        }
+
+    }
+}
--- AOC22/Program.cs
+++ AOC22/Program.cs
@@ -1,13 +1,16 @@
 ﻿using AOC22.Day1;
 using AOC22.Day2;
 using AOC22.Day3;
+using AOC22.Day4;
 
 string solutionPath = Directory.GetCurrentDirectory();
 
 var day1 = new Day1(solutionPath);
 var day2 = new Day2(solutionPath);
 var day3 = new Day3(solutionPath);
+var day4 = new Day4(solutionPath);
 
 day1.Execute();
 day2.Execute();
 day3.Execute();
+day4.Execute();
