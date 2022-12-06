822d677b4c9d618aa78a9908b2538ef3315695f1
Lorenas <urnikislorenas@gmail.com>
2022-12-06 19:50:00 +0200

    complete day 6

---
+++ AOC22/Day6/Day6.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC22.Day6
+{
+    internal class Day6 : Day
+    {
+        public Day6(string path) : base(path)
+        {
+        }
+
+        public override void Execute()
+        {
+            var line = File.ReadAllText(@$"{Path}\data-day6.txt");
+            var decoder = new Decoder(4);
+            for (int i = 0; i < line.Length; i++)
+            {
+                decoder.AddData(line[i]);
+                if (decoder.AllDistinct())
+                {
+                    Console.WriteLine($"Start-of-packet marker is at index {i + 1}");
+                    break;
+                }
+            }
+
+            decoder = new Decoder(14);
+            for (int i = 0; i < line.Length; i++)
+            {
+                decoder.AddData(line[i]);
+                if (decoder.AllDistinct())
+                {
+                    Console.WriteLine($"Start-of-message marker is at index {i + 1}");
+                    break;
+                }
+            }
+        }
+    }
+
+    class Decoder
+    {
+        public Decoder(int size)
+        {
+            Size = size;
+        }
+
+        public int Size { get; private set; }
+
+        public List<char> Data { get; private set; } = new List<char>();
+
+        public void AddData(char c)
+        {
+            Data.Add(c);
+            if (Data.Count > Size)
+                Data.RemoveAt(0);
+        }
+
+        public bool AllDistinct()
+        {
+            return Data.Distinct().Count() == Size;
+        }
+    }
+}
--- AOC22/Program.cs
+++ AOC22/Program.cs
@@ -3,6 +3,7 @@ using AOC22.Day2;
 using AOC22.Day3;
 using AOC22.Day4;
 using AOC22.Day5;
+using AOC22.Day6;
 
 string solutionPath = Directory.GetCurrentDirectory();
 
@@ -11,9 +12,11 @@ var day2 = new Day2(solutionPath);
 var day3 = new Day3(solutionPath);
 var day4 = new Day4(solutionPath);
 var day5 = new Day5(solutionPath);
+var day6 = new Day6(solutionPath);
 
 day1.Execute();
 day2.Execute();
 day3.Execute();
 day4.Execute();
 day5.Execute();
+day6.Execute();
